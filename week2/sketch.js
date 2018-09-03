c9d8d00948f3ad32cde96ebb109be5dbfb0cf716
Ishano <iro4@miami.edu>
2018-09-03 19:43:48 -0400

    new

--- week2/sketch.js
+++ week2/sketch.js
@@ -1,24 +1,22 @@
-function setup() {
-  createCanvas(800, 400);
+function setup(){
+  createCanvas(400, 200);
   background(204);
+  angleMode(DEGREES);
 }
 
 function draw() {
-  //head
-  fill (125, 244, 66);
-  ellipse(100, 50, 120, 60);
-  //mouth
-  fill(65, 160, 244);
-  ellipse(95, 60, 70, 10);
-  //eyes
-  fill((244, 65, 65);
-  ellipse(70,25,15,15);
-  ellipse(125,25,15,15);
-}
-
-function draw() {
-  fill(153);
-  ellipse(132,82,200,200);
-  noFill();
-  ellipse(228,-16,200,200);
+fill(178,34,34)
+beginShape();
+vertex(70,120);
+vertex(75,90);
+vertex(50,100);
+vertex(150,20);
+vertex(350,90);
+vertex(200,90);
+vertex(350,150);
+vertex(140,100);
+vertex(140,150);
+endShape();
+fill(0, 128, 0);
+ellipse(145, 50, 8, 8);
 }
