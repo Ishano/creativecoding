275acc424c084ad4b9f3c7bd36b6501822f8cbf6
Ishano <iro4@miami.edu>
2018-09-08 14:41:44 -0400

    new

---
+++ week3/sketch.js
@@ -0,0 +1,16 @@
+function setup(){
+  createCanvas(480, 400);
+  background(204);
+
+}
+var x= 80;
+var y= 40;
+var d= 70;
+var s= 20;
+
+function draw() {
+rect(20, 60, 30, 30);
+rect(140,60,60, 60);
+rect(300,60,90, 90);
+
+}
